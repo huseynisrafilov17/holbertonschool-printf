d6f379570bc1ccbf517bd9513136be90461d6c22
root <huseynisrafilov17@gmail.com>
2023-11-27 04:36:49 -0800

    no

--- _printf.c
+++ _printf.c
@@ -1,1 +1,32 @@
-Hello
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _printf(const char *format, ...)
+{
+	char *for_length;
+	char *for_malloc;
+	va_list ap;
+	int i, count = 0, length = 0;
+
+	va_start(ap, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == 'c')
+		{
+			for_malloc = malloc(sizeof(char));
+			*for_malloc = va_arg(ap, int);
+			write(1, for_malloc, 1);
+			free(for_malloc);
+		}
+		else if (format[i] == 's')
+		{
+			for_length = va_arg(ap, char *);
+			while(for_length[length] != '\0')
+				length++;
+			//for_malloc = malloc(length * sizeof(char));
+			write(1, for_length, length);
+		}
+	}
+	va_end(ap);
+	return (count);
+}
