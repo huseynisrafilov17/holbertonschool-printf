232c43f186603bc340940b5909d143dda1f78176
fammad <fred.mmov@gmail.com>
2023-11-27 05:23:33 -0800

    no

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@ int _printf(const char *format, ...)
 	int i, count = 0, length = 0;
 
 	va_start(ap, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
 		{
