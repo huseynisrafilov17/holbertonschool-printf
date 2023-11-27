cd9dea18f4a67b4978146cda0227bdc1410b46c1
fammad <fred.mmov@gmail.com>
2023-11-27 06:04:54 -0800

    no

--- _printf.c
+++ _printf.c
@@ -1,6 +1,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+/**
+ * _printf - Function printf.
+ * @format: String.
+ * Return: Count.
+ */
 int _printf(const char *format, ...)
 {
 	char *for_string;
@@ -9,7 +14,7 @@ int _printf(const char *format, ...)
 	int i, count = 0, length = 0;
 
 	va_start(ap, format);
-	for (i = 0;format != NULL && format[i] != '\0'; i++)
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
 		{
@@ -22,11 +27,9 @@ int _printf(const char *format, ...)
 			for_string = va_arg(ap, char *);
 			if (for_string == NULL)
 				for_string = "(null)";
-			while (for_string[length] != '\0')
-				length++;
+			for (length = 0; for_string[length] != '\0'; length++)
 			write(1, for_string, length);
 			count = count + length - 1;
-			length = 0;
 			i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
