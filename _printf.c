ded137118cdcb97c29e76114ab3464fb5b5d3b2f
root <huseynisrafilov17@gmail.com>
2023-11-27 05:06:24 -0800

    no

--- _printf.c
+++ _printf.c
@@ -8,15 +8,15 @@ int _printf(const char *format, ...)
 	int i, count = 0, length = 0;
 
 	va_start(ap, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 1; format[i] != '\0'; i++)
 	{
-		if (format[i] == 'c')
+		if (format[i - 1] == '%' && format[i] == 'c')
 		{
 			for_char = va_arg(ap, int);
 			write(1, &for_char, 1);
 			count++;
 		}
-		else if (format[i] == 's')
+		else if (format[i - 1] == '%' && format[i] == 's')
 		{
 			for_string = va_arg(ap, char *);
 			while(for_string[length] != '\0')
@@ -24,7 +24,7 @@ int _printf(const char *format, ...)
 			write(1, for_string, length);
 			count = count + length;
 		}
-		else if (format[i] == '%')
+		else if (format[i - 1] == '%' && format[i] == '%')
 		{
 			write(1, "%", 1);
 			count++;
