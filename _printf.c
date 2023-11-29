645868cb5f6d8377b3df4e91837e0ef0299adf70
root <huseynisrafilov17@gmail.com>
2023-11-29 04:01:06 -0800

    added d and i to format checking

--- _printf.c
+++ _printf.c
@@ -27,6 +27,32 @@ int _printf_string(char *str)
 	return (write(1, str, length));
 }
 /**
+ * _printf_int - prints int.
+ * @a: integer.
+ * Return: Count.
+ */
+int _printf_int(int a)
+{
+	unsigned int b, count = 0;
+
+	if (a < 0)
+	{
+		count += _print_char('-');
+		b = -1 * a;
+	}
+	else
+		b = a;
+	for (i = 1; (b / i) / 10 > 0; i *= 10)
+		continue;
+	for (; i > 0; i++)
+	{
+		count += _print_char(b / 10 + 48);
+		b = b % 10;
+	}
+
+	return (count);
+}
+/**
  * _printf - Function printf.
  * @format: String.
  * Return: Count.
@@ -47,6 +73,8 @@ int _printf(const char *format, ...)
 				count += _printf_string(va_arg(ap, char *));
 			else if (format[i + 1] == '%')
 				count += _printf_char('%');
+			else if (format[i + 1] == 'i' || format[i + 1] == 'd')
+				count += _printf_int(va_arg(ap, int));
 			else
 			{
 				count += _printf_char(format[i]);
